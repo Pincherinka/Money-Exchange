6ec421af9ab7af33e8d65e4d75e9d8f7863b78a9
tatsiana_shautsova <tatsiana_shautsova@epam.com>
2018-09-29 22:59:54 +0300

    Task 5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+let result = {};
+
+if (currency === 0) {
+    return result;
+    }
+    else if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else {
+
+        const valuesList = {"H":50, "Q":25, "D":10,"N":5, "P":1};
+        result = {"H":0, "Q":0, "D":0,"N":0, "P":0};
+        let restAmount  = currency;
+
+        for (let key in valuesList) {
+
+            let numCoins  = takeWholes(restAmount/valuesList[key]);
+            if (numCoins > 0) {     
+                restAmount = restAmount - valuesList[key] * numCoins   
+                result[key] = numCoins; 
+
+            } else delete result[key];
+        }        
+        return result;  
+    }    
 }
+
+function takeWholes(number) {return  Math.trunc(number);}
+
